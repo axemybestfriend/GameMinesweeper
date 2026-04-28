fbcd0a7fb45a2dc997a1a1be96793603567e5829
Олег <axemybestfriend@mail.ru>
2026-04-28 19:49:35 +0700

    Добавлен тест FieldHasSize (конструктор с размерами и геттеры)

--- test/test.cpp
+++ test/test.cpp
@@ -5,3 +5,9 @@ TEST(MinesweeperTest, ClassCreationTest) {
     Minesweeper game;
     EXPECT_TRUE(true);
 }
+
+TEST(MinesweeperTest, FieldHasSize) {
+    Minesweeper game(10, 20);
+    EXPECT_EQ(game.getWidth(), 10);
+    EXPECT_EQ(game.getHeight(), 20);
+}
