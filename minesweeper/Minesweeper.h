c444e62d54101cb0708cb9d8512fbb6bafcb2fe9
Олег <axemybestfriend@mail.ru>
2026-04-28 20:33:55 +0700

    Реализован конструктор с размерами и геттеры. Тест FieldHasSize проходит

--- minesweeper/Minesweeper.h
+++ minesweeper/Minesweeper.h
@@ -3,4 +3,10 @@
 class Minesweeper {
 public:
     Minesweeper() {}
+    Minesweeper(int w, int h) : width(w), height(h) {}
+    int getWidth() const { return width; }
+    int getHeight() const { return height; }
+private:
+    int width = 0;
+    int height = 0;
 };
