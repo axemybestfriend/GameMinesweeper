689f5c7306f2987856bbda75cb6436dba0fdc824
Олег <axemybestfriend@mail.ru>
2026-04-28 22:10:55 +0700

    Добавлена случайная расстановка мин.Тест MinesArePlacedRandomly проходит

--- minesweeper/Minesweeper.h
+++ minesweeper/Minesweeper.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include <random>
+#include <algorithm>
 
 class Minesweeper {
 public:
@@ -23,12 +25,27 @@ public:
     }
 
     void placeMines() {
-        int placed = 0;
-        for (int y = 0; y < height && placed < minesCount; ++y) {
-            for (int x = 0; x < width && placed < minesCount; ++x) {
-                field[y][x] = true;
-                placed++;
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                field[y][x] = false;
             }
+        }
+
+        int totalCells = width * height;
+        std::vector<int> indices(totalCells);
+        for (int i = 0; i < totalCells; ++i) {
+            indices[i] = i;
+        }
+
+        std::random_device rd;
+        std::mt19937 g(rd());
+        std::shuffle(indices.begin(), indices.end(), g);
+
+        for (int i = 0; i < minesCount; ++i) {
+            int pos = indices[i];
+            int x = pos % width;
+            int y = pos / width;
+            field[y][x] = true;
         }
     }
 
